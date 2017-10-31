a0b2f9b95407749680fe62f0e84f291705da2e96
Michel Ginsbach <sputniklu@gmail.com>
2017-10-31 15:03:45 +0100

    Mise en place du theme marble/ split du index.html en header footer index.php

---
+++ wp-content/themes/marble/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Marble - homepage</title>
+
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css" rel="stylesheet">
+	
+	<!-- polices -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:400,700|Roboto+Slab:400,700" rel="stylesheet">
+	
+	<!-- mes styles -->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css">
+
+</head>
+<body>
+
+	<header class="main-header">
+		<div class="wrapper">
+			<h1 class="logo">Marble</h1>
+			<nav>
+				<ul>
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Journal</a></li>
+					<li><a href="#">Service</a></li>
+					<li><a href="#">Features</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+			<!-- ./main navigation -->
+		</div>
+		<!-- ./wrapper -->
+	</header>
+	<!-- ./main-header -->
+	
+	<main>
+
+<!--	<section class="jumbotron">
+			<div class="wrapper">
+				<h2>We are digital &amp; branding agency based in London.</h2>
+				<h3>We love to turn great ideas into beautiful products.</h3>
+				<a href="#" class="button">See portfolio</a>
+			</div>
+		</section>-->
+		<!-- ./jumbotron -->
---
+++ wp-content/themes/marble/index.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+		<section id="section-icons" class="wrapper">
+			<div class="container">
+				<div class="col">
+					<i class="icon lamp"></i>
+					<h4>Pellentesque</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+				</div>
+				<!-- ./col1 -->
+				<div class="col">
+					<i class="icon clock"></i>
+					<h4>Consectetur</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+				</div>
+				<!-- ./col2 -->
+				<div class="col">
+					<i class="icon flask"></i>
+					<h4>Tristiquet</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+				</div>
+				<!-- ./col3 -->
+				<div class="col">
+					<i class="icon ticket"></i>
+					<h4>Fermentum</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
+				</div>
+				<!-- ./col4 -->
+			</div>
+
+			<hr />
+
+		</section>
+		
+		<section id="section-latest-work" class="wrapper">
+			<h3>Our latest works</h3>
+			<div class="container">
+				<article class="col">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/image1.jpg" alt="Business Card">
+					<h4>Nobis Business Card</h4>
+					<h5>Business Cards, Graphics</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				</article>
+
+				<article class="col">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/image2.jpg" alt="New fun project">
+					<h4>New fun project</h4>
+					<h5>Webdesign, Application</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				</article>
+				
+				<article class="col">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/image3.jpg" alt="Passionaries Branding Cover">
+					<h4>Passionaries Branding Cover</h4>
+					<h5>Branding, Graphic Design</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+				</article>
+			</div>
+		</section>
+<?php get_footer(); ?>
