0a52f7c8fbcda9b4419713a164b73342502f6e37
Michel Ginsbach <sputniklu@gmail.com>
2017-11-03 15:06:29 +0100

    la je suis plus

--- wp-content/themes/marble/front-page.php
+++ wp-content/themes/marble/front-page.php
@@ -1,10 +1,9 @@
 <?php get_header(); ?>
-<!-- ..............................................................................-->
+
 		<section id="section-icons" class="wrapper">
 			<div class="container">
 				<div class="col">
 					<i class="icon lamp"></i>
-					<h1>Front-page.php</h1>
 					<h4>Pellentesque</h4>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, quasi facere, animi maxime natus cupiditate</p>
 				</div>
@@ -33,30 +32,47 @@
 
 		</section>
 		
-		<section id="section-latest-work" class="wrapper">
-			<h3>Our latest works</h3>
-			<div class="container">
-				<article class="col">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/image1.jpg" alt="Business Card">
-					<h4>Nobis Business Card</h4>
-					<h5>Business Cards, Graphics</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</article>
 
-				<article class="col">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/image2.jpg" alt="New fun project">
-					<h4>New fun project</h4>
-					<h5>Webdesign, Application</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</article>
+		<!-- requête à ma base de donnée avec la fonction WP_Query() -->
+		<?php 
+		$args = array(
+			'post_type'		 => 'post',
+			'posts_per_page' => 3
+		);
+		// the query
+		$the_query = new WP_Query( $args ); ?>
+		 
+		<!-- si j'ai des résultats -->
+		<?php if ( $the_query->have_posts() ) : ?>
+			
+			<section id="section-latest-work" class="wrapper">
 				
-				<article class="col">
-					<img src="<?php echo get_template_directory_uri(); ?>/img/image3.jpg" alt="Passionaries Branding Cover">
-					<h4>Passionaries Branding Cover</h4>
-					<h5>Branding, Graphic Design</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-				</article>
-			</div>
-		</section>
-<!-- ..............................................................................-->
+				<h3>Our latest works</h3>
+				
+				<div class="container">
+					<!-- boucle personnalisée -->
+					<?php while ( $the_query->have_posts() ) : 
+						$the_query->the_post(); ?>
+						<article class="col">
+							<?php if(has_post_thumbnail()) : ?>
+								<?php the_post_thumbnail( 'home-thumbnail'); ?><!-- 380x270 -->
+							<?php else: ?>
+								<img src="<?= get_template_directory_uri(); ?>/img/image1.jpg" alt="Business Card">
+							<?php endif; ?>
+							<h4><?php the_title(); ?></h4>
+							<!-- <h5><?php /*the_category(', ');*/ ?></h5> -->
+							<p><?php the_excerpt(); ?></p>
+						</article>
+
+					<!-- fin de boucle -->
+					<?php endwhile; ?>
+				</div>
+			</section>
+
+		<!-- fin de si j'ai des résultats -->
+		<?php endif; ?>
+
+		<?php wp_reset_postdata(); ?>
+
+
 <?php get_footer(); ?>
--- wp-content/themes/marble/functions.php
+++ wp-content/themes/marble/functions.php
@@ -27,6 +27,7 @@ function marble_setup(){
     add_image_size( 'list-tumbnail', 250, 180, true );
     add_image_size( 'signle-thumbnails', 400, 120, true );
     add_image_size( 'taille-baniere', 1000, 300, true );
+    add_image_size( 'home-thumbnail', 380, 270, true );
     
     //fonctionalite pour les image dans admin
     add_theme_support( 'post-thumbnails', array( 'post' )); 
@@ -90,6 +91,159 @@ function marble_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'marble_scripts' );
 
+
+
+/* costom post portofolio */ 
+
+// Register Custom Post Type
+function portfolio_post_type() {
+
+    $labels = array(
+        'name'                  => _x( 'Portfolios', 'Post Type General Name', 'marble' ),
+        'singular_name'         => _x( 'Portfolio', 'Post Type Singular Name', 'marble' ),
+        'menu_name'             => __( 'Portfolios', 'marble' ),
+        'name_admin_bar'        => __( 'Portfolio', 'marble' ),
+        'archives'              => __( 'Item Archives', 'marble' ),
+        'attributes'            => __( 'Item Attributes', 'marble' ),
+        'parent_item_colon'     => __( 'Parent Portfolio:', 'marble' ),
+        'all_items'             => __( 'All portfolios', 'marble' ),
+        'add_new_item'          => __( 'Add New Portfolio', 'marble' ),
+        'add_new'               => __( 'New portfolio', 'marble' ),
+        'new_item'              => __( 'New Item', 'marble' ),
+        'edit_item'             => __( 'Edit Portfolio', 'marble' ),
+        'update_item'           => __( 'Update portfolio', 'marble' ),
+        'view_item'             => __( 'View portfolio', 'marble' ),
+        'view_items'            => __( 'View Items', 'marble' ),
+        'search_items'          => __( 'Search portfolio', 'marble' ),
+        'not_found'             => __( 'No portfolio found', 'marble' ),
+        'not_found_in_trash'    => __( 'No portfolios found in Trash', 'marble' ),
+        'featured_image'        => __( 'Featured Image', 'marble' ),
+        'set_featured_image'    => __( 'Set featured image', 'marble' ),
+        'remove_featured_image' => __( 'Remove featured image', 'marble' ),
+        'use_featured_image'    => __( 'Use as featured image', 'marble' ),
+        'insert_into_item'      => __( 'Insert into item', 'marble' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this item', 'marble' ),
+        'items_list'            => __( 'Items list', 'marble' ),
+        'items_list_navigation' => __( 'Items list navigation', 'marble' ),
+        'filter_items_list'     => __( 'Filter items list', 'marble' ),
+    );
+    $rewrite = array(
+        'slug'                  => 'portfolio',
+        'with_front'            => true,
+        'pages'                 => true,
+        'feeds'                 => true,
+    );
+    $args = array(
+        'label'                 => __( 'Portfolio', 'marble' ),
+        'description'           => __( 'Portfolio information page', 'marble' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'custom-fields', ),
+        'taxonomies'            => array( 'color', 'type' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 20,
+        'menu_icon'             => 'dashicons-art',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => 'portfolioreferences',
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'rewrite'               => $rewrite,
+        'capability_type'       => 'post',
+        'show_in_rest'          => false,
+    );
+    register_post_type( 'portfolio', $args );
+
+}
+add_action( 'init', 'portfolio_post_type', 0 );
+/*
+definition taxonomie du portofolio
+*/
+
+// Register Custom Taxonomy
+function portfolio_color_taxonomy() {
+
+    $labels = array(
+        'name'                       => _x( 'Colors', 'Taxonomy General Name', 'marble' ),
+        'singular_name'              => _x( 'Color', 'Taxonomy Singular Name', 'marble' ),
+        'menu_name'                  => __( 'Color', 'marble' ),
+        'all_items'                  => __( 'All Items', 'marble' ),
+        'parent_item'                => __( 'Parent Item', 'marble' ),
+        'parent_item_colon'          => __( 'Parent Item:', 'marble' ),
+        'new_item_name'              => __( 'New Item Name', 'marble' ),
+        'add_new_item'               => __( 'Add New Item', 'marble' ),
+        'edit_item'                  => __( 'Edit Item', 'marble' ),
+        'update_item'                => __( 'Update Item', 'marble' ),
+        'view_item'                  => __( 'View Item', 'marble' ),
+        'separate_items_with_commas' => __( 'Separate items with commas', 'marble' ),
+        'add_or_remove_items'        => __( 'Add or remove items', 'marble' ),
+        'choose_from_most_used'      => __( 'Choose from the most used', 'marble' ),
+        'popular_items'              => __( 'Popular Items', 'marble' ),
+        'search_items'               => __( 'Search Items', 'marble' ),
+        'not_found'                  => __( 'Not Found', 'marble' ),
+        'no_terms'                   => __( 'No items', 'marble' ),
+        'items_list'                 => __( 'Items list', 'marble' ),
+        'items_list_navigation'      => __( 'Items list navigation', 'marble' ),
+    );
+    $args = array(
+        'labels'                     => $labels,
+        'hierarchical'               => false,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => true,
+    );
+    register_taxonomy( 'color', array( 'portfolio' ), $args );
+
+}
+add_action( 'init', 'portfolio_color_taxonomy', 0 );
+
+/*
+taxonomie type
+*/
+
+// Register Custom Taxonomy
+function portfolio_type_taxonomy() {
+
+    $labels = array(
+        'name'                       => _x( 'Types', 'Taxonomy General Name', 'marble' ),
+        'singular_name'              => _x( 'Type', 'Taxonomy Singular Name', 'marble' ),
+        'menu_name'                  => __( 'Type', 'marble' ),
+        'all_items'                  => __( 'All Items', 'marble' ),
+        'parent_item'                => __( 'Parent Item', 'marble' ),
+        'parent_item_colon'          => __( 'Parent Item:', 'marble' ),
+        'new_item_name'              => __( 'New Item Name', 'marble' ),
+        'add_new_item'               => __( 'Add New Item', 'marble' ),
+        'edit_item'                  => __( 'Edit Item', 'marble' ),
+        'update_item'                => __( 'Update Item', 'marble' ),
+        'view_item'                  => __( 'View Item', 'marble' ),
+        'separate_items_with_commas' => __( 'Separate items with commas', 'marble' ),
+        'add_or_remove_items'        => __( 'Add or remove items', 'marble' ),
+        'choose_from_most_used'      => __( 'Choose from the most used', 'marble' ),
+        'popular_items'              => __( 'Popular Items', 'marble' ),
+        'search_items'               => __( 'Search Items', 'marble' ),
+        'not_found'                  => __( 'Not Found', 'marble' ),
+        'no_terms'                   => __( 'No items', 'marble' ),
+        'items_list'                 => __( 'Items list', 'marble' ),
+        'items_list_navigation'      => __( 'Items list navigation', 'marble' ),
+    );
+    $args = array(
+        'labels'                     => $labels,
+        'hierarchical'               => true,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => true,
+    );
+    register_taxonomy( 'type', array( 'portfolio' ), $args );
+
+}
+add_action( 'init', 'portfolio_type_taxonomy', 0 );
 //stoper une fonction
 //remove_action('after_setup_theme', 'marble_setup');
 
